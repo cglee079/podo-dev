0b23c0cecd59c082407489f3b702ad303f137b9d
cglee079 <cglee079@zuminternet.com>
2019-07-04 14:02:44 +0900

    init

---
+++ backend/src/main/java/com/cglee079/pododev/PodoDevApplication.java
@@ -0,0 +1,13 @@
+package com.cglee079.pododev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PodoDevApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PodoDevApplication.class, args);
+    }
+
+}
