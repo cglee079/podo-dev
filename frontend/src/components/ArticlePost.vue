6a59aba2b32780d2f72cc2eba9d84f964d1db130
cglee079 <cglee079@zuminternet.com>
2019-07-09 13:23:08 +0900

    게시글 업로드 제작중..

--- frontend/src/components/ArticlePost.vue
+++ frontend/src/components/ArticlePost.vue
@@ -1,12 +1,48 @@
 <template>
   <div id="wrapArticlePost">
+    <ckeditor :editor="editor" v-model="editorData" :config="editorConfig"></ckeditor>
   </div>
 </template>
 
 <script>
+  // ⚠️ NOTE: We don't use @ckeditor/ckeditor5-build-classic any more!
+  // Since we're building CKEditor from source, we use the source version of ClassicEditor.
+  import ClassicEditor from '@ckeditor/ckeditor5-editor-classic/src/classiceditor';
+
+  import EssentialsPlugin from '@ckeditor/ckeditor5-essentials/src/essentials';
+  import BoldPlugin from '@ckeditor/ckeditor5-basic-styles/src/bold';
+  import ItalicPlugin from '@ckeditor/ckeditor5-basic-styles/src/italic';
+  import LinkPlugin from '@ckeditor/ckeditor5-link/src/link';
+  import ParagraphPlugin from '@ckeditor/ckeditor5-paragraph/src/paragraph';
+
   export default {
-    name: 'ArticlePost'
-  }
+    name: 'app',
+    data() {
+      return {
+        editor: ClassicEditor,
+        editorData: '<p>Content of the editor.</p>',
+        editorConfig: {
+          plugins: [
+            EssentialsPlugin,
+            BoldPlugin,
+            ItalicPlugin,
+            LinkPlugin,
+            ParagraphPlugin
+          ],
+
+          toolbar: {
+            items: [
+              'bold',
+              'italic',
+              'link',
+              'undo',
+              'redo'
+            ]
+          }
+        }
+      };
+    }
+  };
 </script>
 
 <style scoped>
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,19 +1,13 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-import App from './App'
+import App from './App.vue'
 import router from './router'
-import axios from 'axios'
-import moment from 'moment'
+import CKEditor from '@ckeditor/ckeditor5-vue';
+
+Vue.use( CKEditor );
 
 Vue.config.productionTip = false
-Vue.prototype.$axios = axios
-Vue.prototype.$moment = moment
 
-/* eslint-disable no-new */
 new Vue({
-  el: '#app',
   router,
-  components: { App },
-  template: '<App/>'
-})
+  render: h => h(App),
+}).$mount('#app')
