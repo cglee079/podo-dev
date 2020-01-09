39fe335b61a77bff31eb7f2b84b0eaec713ea619
cglee079 <cglee079@zuminternet.com>
2020-01-09 17:19:29 +0900

    feature : 유저 데이터 정보 변경

--- podo-dev-web/frontend/store/user/mutations.js
+++ podo-dev-web/frontend/store/user/mutations.js
@@ -2,14 +2,12 @@ export default {
     doLogin(state, user) {
         state.isLogin = true;
         state.userinfo.username = user.username;
-        state.userinfo.email = user.email;
         state.userinfo.profileImage = user.profileImage;
         state.userinfo.isAdmin = user.isAdmin;
     },
     doLogout(state) {
         state.isLogin = false;
         state.userinfo.username = "";
-        state.userinfo.email = "";
         state.userinfo.profileImage = "";
         state.userinfo.isAdmin = false;
     }
--- podo-dev-web/frontend/store/user/state.js
+++ podo-dev-web/frontend/store/user/state.js
@@ -2,7 +2,6 @@ export default () => ({
     isLogin: false,
     userinfo: {
         name: "",
-        email: "",
         picture: "",
         isAdmin: false
     }
