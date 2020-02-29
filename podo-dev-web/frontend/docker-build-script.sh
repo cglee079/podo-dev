797a4a53d1eaa457d8b6b284566ab58e589968ba
Podo <cglee079@gmail.com>
2020-02-29 22:09:22 +0900

    hotfix : 실서버 데이터 불일치 fix

--- podo-dev-web/frontend/docker-build-script.sh
+++ podo-dev-web/frontend/docker-build-script.sh
@@ -4,5 +4,5 @@ docker stop podo-dev-frontend
 docker rm podo-dev-frontend
 docker rmi podo-dev-frontend:1.1 --force
 docker build --tag podo-dev-frontend:1.1 .
-docker run --name podo-dev-frontend -p 20080:3000 -v /volume1/docker/podo-dev/podo-dev-frontend/static:/app/static podo-dev-frontend:1.1
+docker run --name podo-dev-frontend -p 20080:8000 -v /volume1/docker/podo-dev/podo-dev-frontend/static:/app/static podo-dev-frontend:1.1
 
