76affce5822324ef41cfd48324ab565111142c7e
cglee079 <cglee079@gmail.com>
2020-04-08 21:46:17 +0900

    feature : podo-dev-stroage ci setting

--- podo-dev-backend/src/main/java/com/podo/pododev/PodoDevWebApplication.java
+++ podo-dev-backend/src/main/java/com/podo/pododev/PodoDevWebApplication.java
@@ -2,8 +2,6 @@ package com.podo.pododev;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.telegram.telegrambots.ApiContextInitializer;
 
--- podo-dev-storage/src/test/java/com/podo/pododev/PodoDevStorageApplicationTests.java
+++ podo-dev-storage/src/test/java/com/podo/pododev/PodoDevStorageApplicationTests.java
@@ -1,16 +1,16 @@
 package com.podo.pododev;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
+@ActiveProfiles("test")
 @SpringBootTest
 public class PodoDevStorageApplicationTests {
 
     @Test
-    public void contextLoads() {
+    void contextLoads() {
     }
 
 }
