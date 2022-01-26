6de124fb2a2b8ff88c01db87d2f2645fb6a1ed09
podo.dev <cglee079@gmail.com>
2022-01-26 14:18:24 +0900

    build gradle

--- podo-dev-backend/src/main/java/com/podo/pododev/backend/global/web/HttpServerConfig.java
+++ podo-dev-backend/src/main/java/com/podo/pododev/backend/global/web/HttpServerConfig.java
@@ -1,25 +1,25 @@
-package com.podo.pododev.backend.global.web;
-
-import org.apache.catalina.connector.Connector;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
-import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
-import org.springframework.context.annotation.Bean;
-import org.springframework.stereotype.Component;
-
-@Component
-public class HttpServerConfig {
-
-    @Value("${server.http.port:8080}")
-    private Integer httpPort;
-
-    @Bean
-    public ServletWebServerFactory servletContainer() {
-        Connector connector = new Connector(TomcatServletWebServerFactory.DEFAULT_PROTOCOL);
-        connector.setPort(httpPort);
-
-        TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory();
-        tomcat.addAdditionalTomcatConnectors(connector);
-        return tomcat;
-    }
-}
+//package com.podo.pododev.backend.global.web;
+//
+//import org.apache.catalina.connector.Connector;
+//import org.springframework.beans.factory.annotation.Value;
+//import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+//import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.stereotype.Component;
+//
+//@Component
+//public class HttpServerConfig {
+//
+//    @Value("${server.http.port:8080}")
+//    private Integer httpPort;
+//
+//    @Bean
+//    public ServletWebServerFactory servletContainer() {
+//        Connector connector = new Connector(TomcatServletWebServerFactory.DEFAULT_PROTOCOL);
+//        connector.setPort(httpPort);
+//
+//        TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory();
+//        tomcat.addAdditionalTomcatConnectors(connector);
+//        return tomcat;
+//    }
+//}
